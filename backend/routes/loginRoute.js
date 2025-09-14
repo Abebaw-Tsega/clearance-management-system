59a5645a34fd1e43abb5c5f29dff599debbf0154
netsanetabuhay <netsanetabuhay@gmail.com>
2025-09-14 07:40:12 +0300

    Removed backend folder from project

--- backend/routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, changePassword, staffLogin } = require('../controller/login');
-
-router.post('/login', login);
-router.post('/change-password', changePassword);
-// router.post('/staff/login', staffLogin);
-
-module.exports = router;
